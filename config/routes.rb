93144128e217448f361a48c289ab920ad3f24099
Tyler Mumford <tyler.mumford@icloud.com>
2022-01-12 23:29:14 -0700

    Use helpers for links

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
 
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
+  # get "/articles", to: "articles#index"
+  # get "/articles/:id", to: "articles#show"
 end
