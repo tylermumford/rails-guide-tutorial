08a2763c25b95cfcd2e5d5682a9b2be813588e6a
Tyler Mumford <tyler.mumford@icloud.com>
2022-01-14 07:34:22 -0700

    Add comments and refactor out some partials.

---
+++ bugs.md
@@ -0,0 +1,3 @@
+# Bugs
+
+- #1 When the page is reloaded, if there are comments, the form is populated with the most recent comment's info. I would expect the form to stay empty.
--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
 
-  resources :articles
-  # get "/articles", to: "articles#index"
-  # get "/articles/:id", to: "articles#show"
+  resources :articles do
+    resources :comments
+  end
+
 end
