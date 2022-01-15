273b3cdd692db0203c55013b8d974ff8e8439ead
Tyler Mumford <tyler.mumford@icloud.com>
2022-01-14 22:48:31 -0700

    Add basic auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "tyler", password: "sublime", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
